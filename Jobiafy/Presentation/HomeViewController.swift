29333058307063a66b383d67632e8e293f1985a5
Fadysameh2020 <fadysameh10@gmail.com>
2024-05-18 05:18:39 +0300

    Sourcetree commit

--- Jobiafy/Presentation/HomeViewController.swift
+++ Jobiafy/Presentation/HomeViewController.swift
@@ -14,6 +14,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let firstCommit = 12
     }
     // swiftlint:disable all
 
